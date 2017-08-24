4021c88b110d392cc52ed25006d4da4386b7ed43
Eva Millán <evamillanmartin@gmail.com>
2017-08-24 22:19:52 +0200

    List all favorited notes

--- api/controllers/noteController.js
+++ api/controllers/noteController.js
@@ -12,7 +12,9 @@ exports.list_all_notes = function(req, res) {
 };
 
 exports.create_a_note = function(req, res) {
-  var new_note = new Note(req.body);
+  var new_note = new Note({
+    text: req.body.text
+  });
   new_note.save(function(err, note) {
     if (err)
       res.send(err);
@@ -43,5 +45,13 @@ exports.delete_a_note = function(req, res) {
     if (err)
       res.send(err);
     res.json({ message: 'Note successfully deleted' });
+  });
+};
+
+exports.find_favorites = function(req, res) {
+  Note.find({favorite: true}, function(err, note) {
+    if (err)
+      res.send(err);
+    res.json(note);
   });
 };
